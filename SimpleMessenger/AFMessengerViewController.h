50ea475c07b82fb50c6e476b8c4f76ff33531dbc
alexander <fairk19@gmail.com>
2016-04-03 13:47:17 +0300

    Добавлен класс AFMessengerViewController и заданы параметры TableView

---
+++ SimpleMessenger/AFMessengerViewController.h
@@ -0,0 +1,18 @@
+//
+//  AFMessengerViewController.h
+//  SimpleMessenger
+//
+//  Created by Александр on 03.04.16.
+//  Copyright © 2016 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class AFChatTableView;
+
+@interface AFMessengerViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+
+@end
